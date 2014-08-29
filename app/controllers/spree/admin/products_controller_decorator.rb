021948fc9f2a560524858a5b404d4ab28e659166
Pedro Quintero Rojas <pqr@uh.cu>
2014-08-28 22:50:08 -0400

    fixing a bug

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,11 +1,18 @@
 Spree::Admin::ProductsController.class_eval do
+
   def neighbors
     locatable = @product.location
-    locatable.neighbors_settings = locatable.neighbors_settings || Spree::NeighborsSettings.create(:location_id => locatable.id ,:radius => 0, :count => 0, :sort => 1)
-    @neighbors_settings = locatable.neighbors_settings
-    neighbors = Spree::Neighbors.where(neighbors_settings_id: @neighbors_settings.id).map { |neighbor| neighbor.location_id}
-    @locatables = Spree::Location.where(locatable_type: 'Spree::Product').where.not(locatable_id: @product.id).where.not(id: neighbors).map { |location| [location.locatable.name, location.id]}
-    @neighbors_locatables = Spree::Neighbors.where(neighbors_settings_id: @neighbors_settings.id).map { |neighbor| [Spree::Location.find(neighbor.location_id).locatable.name, neighbor.location_id, neighbor]}
+    default_options = {:location_id => locatable.id ,:radius => 0, :count => 0, :sort => 1}
+    default_neighbors_settins = Spree::NeighborsSettings.create(default_options)
+    @neighbors_settings = locatable.neighbors_settings = locatable.neighbors_settings || default_neighbors_settings
+    neighbor_location_ids = Spree::Neighbors.where(neighbors_settings_id: @neighbors_settings.id).pluck(:location_id)
+    locatables = Spree::Location.where(locatable_type: 'Spree::Product')
+    locatables = locatables.where.not(locatable_id: @product.id)
+    locatables = locatables.where.not(id: neighbor_location_ids)
+    @locatables = locatables.map { |location| [location.locatable.name, location.id]}
+    neighbors_locatables = Spree::Neighbors.where(neighbors_settings_id: @neighbors_settings.id)
+    neighbors_locatables = neighbors_locatables.includes([{:location => {:locatable => :translations}}])
+    @neighbors_locatables = neighbors_locatables.map { |neighbor| [neighbor.location.locatable.name, neighbor.location_id, neighbor]}
     @properties = Spree::Property.all.map{|property| [property.name, property.id]}
     @sorting_options = [['By Distance', 1], ['By Name', 2]]
     @sorting_selected = @neighbors_settings.sort

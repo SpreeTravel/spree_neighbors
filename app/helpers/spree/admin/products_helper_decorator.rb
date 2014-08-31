5073e9e435eced778dec276c82c569f98d4abd7e
Pedro Quintero Rojas <pqr@uh.cu>
2014-08-30 22:40:53 -0400

    bug fixing

--- app/helpers/spree/admin/products_helper_decorator.rb
+++ app/helpers/spree/admin/products_helper_decorator.rb
@@ -1,45 +1,37 @@
 Spree::ProductsHelper.module_eval do
+
   def get_name_from_locatable(item)
     location_item = Spree::Location.find_by_id(item)
     location_item.locatable.name
   end
 
   def get_neighbors_pins_coordinates(item)
-    neighbors_settings = item.neighbors_settings
-    result = []
-    unless neighbors_settings.nil?
-      neighbors_ids = Spree::Neighbors.where(neighbors_settings_id: neighbors_settings.id).map { |neighbor| neighbor.location_id}
-      nearby_items = neighbors_settings.radius > 0 ? Spree::Location.where(locatable_type: item.locatable_type).where.not(id: item.id).where.not(id: neighbors_ids).near([item.latitude, item.longitude], neighbors_settings.radius).map { |locatable| {:latitude => locatable.latitude, :longitude => locatable.longitude, :distance => item.distance_from([locatable.latitude, locatable.longitude]), :locatable_id => locatable.id } } : []
-      neighbors = Spree::Neighbors.where(neighbors_settings_id: neighbors_settings.id).map { |neighbor| {:latitude => neighbor.location.latitude, :longitude => neighbor.location.longitude, :distance => item.distance_from([neighbor.location.latitude, neighbor.location.longitude]), :locatable_id => neighbor.location.id } }
-      products_prop = neighbors_settings.neighbors_by_property.nil? ? [] : neighbors_settings.neighbors_by_property.value == "" ? [] : Spree::ProductProperty.where(property_id: neighbors_settings.neighbors_by_property.property_id, value: neighbors_settings.neighbors_by_property.value).map{ |product_prop| product_prop.product }
-      neighbors_by_prop = []
-      products_prop.each do |prod|
-        unless prod.location.nil?
-          neighbors_by_prop << {:latitude => prod.location.latitude, :longitude => prod.location.longitude, :distance => item.distance_from([prod.location.latitude, prod.location.longitude]), :locatable_id => prod.location.id }
-        end
-      end
-      union = nearby_items | neighbors | neighbors_by_prop
-
-      #The sorting bussiness goes here
-
-      case neighbors_settings.sort
-      when 2
-        sorted = union.sort_by { |neighbor| Spree::Location.find(neighbor[:locatable_id]).locatable.name }
-      when 1
-        sorted = union.sort_by { |neighbor| neighbor[:distance] }
-      else
-        sorted = union
-      end
-
-      result = sorted
-
-      if neighbors_settings.count > 0
-        #it does'nt matter if the count is bigger than the amount of items it gets them all
-        result = sorted.take(neighbors_settings.count)
-      end
+    by_distance = item.neighbors_ids_by_distance
+    by_choice = item.neighbors_ids_by_choice
+    by_property = item.neighbors_ids_by_property
+    union = by_choise
+    if by_property
+      union = union | by_distance & by_property
+    else
+      union = union | by_distance
+    end
+    list = Spree::Location.where(:id => union).compact.map do |location|
+      {
+        :latitude => location.latitude,
+        :longitude => neighbor.location.longitude,
+        :distance => item.distance_from([location.latitude, location.longitude]),
+        :locatable_id => location.id
+      }
     end
 
-    result
+    case neighbors_settings.sort
+    when 2 then sorted = list.sort_by { |neighbor| Spree::Location.find(neighbor[:locatable_id]).locatable.name }
+    when 1 then sorted = list.sort_by { |neighbor| neighbor[:distance] }
+    else sorted = list
+    end
+
+    sorted = sorted[0,neighbors_settings.count] if neighbors_settings.count > 0
+    sorted
   end
 
   def neighbors_map(product)
--- app/models/spree/location_decorator.rb
+++ app/models/spree/location_decorator.rb
@@ -4,4 +4,35 @@ Spree::Location.class_eval do
   #Fields for the geocoder gem
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode
+
+  def neighbors_ids_by_distance
+    settings = self.neighbors_settings
+    return [] if settings.nil?
+    return [] if settings.radius == 0
+    nearby_items = Spree::Location.where(locatable_type: self.locatable_type)
+    nearby_items = nearby_items.where.not(id: self.id)
+    nearby_items = nearby_items.near([self.latitude, self.longitude], settings.radius)
+    #nearby_items = nearby_items.limit(settings.count)
+    nearby_items = nearby_items.pluck(:id)
+    nearby_items
+  end
+
+  def neighbors_ids_by_choise
+    settings = self.neighbors_settings
+    return [] if settings.nil?
+    neighbors_ids = Spree::Neighbors.where(neighbors_settings_id: settings.id)
+    #neighbots_ids = neighbors_ids.limit(settings.count)
+    neighbors_ids = neighbors_ids.pluck(:location_id)
+    neighbors_ids
+  end
+
+  def neighbors_ids_by_property
+    settings = self.neighbors_settings
+    return nil if settings.nil?
+    return nil if settings.neighbors_by_property.nil?
+    pp = Spree::ProductProperty.where(property_id: settings.neighbors_by_property.property_id)
+    pp = pp.where(value: settings.neighbors_by_property.value) if settings.neighbors_by_property.value.present?
+    neighbors_ids = pp.map { |p| p.product.location.id rescue nil }.compact
+    neighbors_ids
+  end
 end

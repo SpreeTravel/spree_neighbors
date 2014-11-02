70a6f018a294ae36ead5603fd873f1b25f37c948
Pedro Quintero Rojas <pqr@uh.cu>
2014-11-02 14:16:44 -0500

    moving partial to its right place

--- app/overrides/add_neighbors_section_to_product_show.rb
+++ app/overrides/add_neighbors_section_to_product_show.rb
@@ -2,6 +2,6 @@ Deface::Override.new(
   :virtual_path => "spree/products/show",
   :name => "add_neighbors_section_to_product_show",
   :insert_after => '[data-hook="product_left_part_wrap"]',
-  :partial => "spree/admin/products/neighbors_map",
+  :partial => "spree/products/neighbors_map",
   :disabled => false
 )
